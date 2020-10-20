b06e57725a28a30bd7c405081ecfd82b79f2eabc
mery-takano <atsushi.takano@mery.co.jp>
2020-10-20 18:07:37 +0900

    add setup file

---
+++ setup-shell/vscode.sh
@@ -0,0 +1,6 @@
+code --install-extension donjayamanne.githistory
+code --install-extension eamodio.gitlens
+code --install-extension emilast.LogFileHighlighter
+code --install-extension mechatroner.rainbow-csv
+code --install-extension shardulm94.trailing-spaces
+code --install-extension vscode-icons-team.vscode-icons
