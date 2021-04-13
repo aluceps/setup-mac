94b0259f826b6bbfaa7be9301067047a4764bb7a
janedoe <janedoe@mercury.lan>
2021-04-13 16:49:15 +0900

    add setup file for fisher

---
+++ setup-shell/fisher.sh
@@ -0,0 +1,6 @@
+# install fisher
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+# add plugins
+fisher install oh-my-fish/theme-bobthefish
+fisher install oh-my-fish/plugin-peco
