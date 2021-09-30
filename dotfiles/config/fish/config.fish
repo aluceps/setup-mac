c864a6af6d9ad379c802a3bcdab76d98c63e6cb3
janedoe <janedoe@mercury.local>
2021-09-30 16:34:41 +0900

    remove keybindings

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -14,10 +14,3 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g JAVA_HOME (/usr/libexec/java_home -v "1.8") $JAVA_HOME
 set -g PATH ~/Library/Android/sdk/platform-tools $PATH
 set -g PATH ~/ghq/github.com.private/aluceps/fish-shell/function $PATH
-
-# peco & history
-function fish_user_key_bindings
-	bind \cr 'peco_select_history (commandline -b)'
-	bind \cl 'peco_select_ghq_list'
-	bind \cb 'peco_select_git_checkout'
-end
