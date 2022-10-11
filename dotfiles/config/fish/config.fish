17c3c0b3639d9a9c1f28d1b61ac5b3f172f3e0fc
atsushi.takano <atsushi.takano@every.tv>
2022-10-11 16:06:14 +0900

    update config

--- dotfiles/config/fish/config.fish
+++ dotfiles/config/fish/config.fish
@@ -11,6 +11,14 @@ alias convert2gif="convert_to_gif"
 
 # path
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g JAVA_HOME (/usr/libexec/java_home -v "1.8") $JAVA_HOME
+# set -g JAVA_HOME (/usr/libexec/java_home -v "11") $JAVA_HOME
 set -g PATH ~/Library/Android/sdk/platform-tools $PATH
 set -g PATH ~/ghq/github.com.private/aluceps/fish-shell/function $PATH
+
+# fish
+set -U theme_display_date no
+set -U theme_display_cmd_duration no
+
+# fish-ghq
+set GHQ_SELECTOR peco
+set GHQ_SELECTOR_OPTS --layout=bottom-up
