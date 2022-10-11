d876d6408ac0660bdcd0d5cb1d5775a18c608724
atsushi.takano <atsushi.takano@every.tv>
2022-10-11 16:04:01 +0900

    update config

--- setup-shell/fisher.sh
+++ setup-shell/fisher.sh
@@ -4,3 +4,4 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 # add plugins
 fisher install oh-my-fish/theme-bobthefish
 fisher install oh-my-fish/plugin-peco
+fisher install decors/fish-ghq
