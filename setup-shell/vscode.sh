32395efe4e95f8e4ba60aacd767778a77625ef96
aluceps <aluceps@gmail.com>
2022-12-14 15:13:14 +0900

    update setup file

--- setup-shell/vscode.sh
+++ setup-shell/vscode.sh
@@ -2,5 +2,7 @@ code --install-extension donjayamanne.githistory
 code --install-extension eamodio.gitlens
 code --install-extension emilast.LogFileHighlighter
 code --install-extension mechatroner.rainbow-csv
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension naumovs.color-highlight
 code --install-extension shardulm94.trailing-spaces
 code --install-extension vscode-icons-team.vscode-icons
